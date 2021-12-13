939ab343950343d02c070f00f9682baef048173e
Nicholas Nethercote <n.nethercote@gmail.com>
2021-12-14 09:36:17 +1100

    Remove dhat-rs docs.
    0.3.0-pre.1 docs are are now on docs.rs.

--- dhat-rs-0.3-draft-docs/implementors/core/cmp/trait.Eq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["dhat"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"dhat/struct.HeapStats.html\" title=\"struct dhat::HeapStats\">HeapStats</a>","synthetic":false,"types":["dhat::HeapStats"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"dhat/struct.AdHocStats.html\" title=\"struct dhat::AdHocStats\">AdHocStats</a>","synthetic":false,"types":["dhat::AdHocStats"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- dhat-rs-0.3-draft-docs/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["dhat"] = {"name":"","files":["lib.rs"]};
-createSourceSidebar();
